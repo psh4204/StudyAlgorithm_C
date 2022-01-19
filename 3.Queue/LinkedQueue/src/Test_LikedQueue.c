1adaf62ac65bb38757e8dfe568d5a5eb63a217c9
박시환 <52143252+psh4204@users.noreply.github.com>
2022-01-19 22:41:30 +0900

    Create Test_LikedQueue.c

---
+++ 3.Queue/LinkedQueue/src/Test_LikedQueue.c
@@ -0,0 +1,26 @@
+
+#include "LinkdeQueue.h"
+
+int main(void) {
+	Node* Popped;
+	LinkedQueue *Queue;
+
+	LQ_CreateQueue(&Queue);
+
+	LQ_Enqueue(Queue, LQ_CreateNode("abc"));
+	LQ_Enqueue(Queue, LQ_CreateNode("def"));
+	LQ_Enqueue(Queue, LQ_CreateNode("ghi"));
+	LQ_Enqueue(Queue, LQ_CreateNode("jkl"));
+
+	printf("Queue Size : %d\n", Queue->Count);
+
+	while (LQ_IsEmpty(Queue) == 0) {
+		Popped = LQ_Dequeue(Queue);
+		printf("Dequeue: %s\n", Popped->Data);
+		LQ_DestoryNode(Popped);
+	}
+
+	LQ_DestroyQueue(Queue);
+
+	return 0;
+}
